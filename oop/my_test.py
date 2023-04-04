ba49c4995205a0bebc84091db91dd68d34bfb44a
nurles07 <nurlesmurzabekov@gmail.com>
2023-04-04 12:19:10 +0600

    api

--- oop/my_test.py
+++ oop/my_test.py
@@ -183,3 +183,28 @@
 
 # print(sum)
 
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+        
+# def addTwoNumbers(l1: ListNode, l2: ListNode) -> ListNode:
+#     dummy = ListNode(0)
+#     current = dummy
+#     carry = 0
+    
+#     while l1 or l2 or carry:
+#         sum = carry
+#         if l1:
+#             sum += l1.val
+#             l1 = l1.next
+#         if l2:
+#             sum += l2.val
+#             l2 = l2.next
+            
+#         carry = sum // 10
+#         current.next = ListNode(sum % 10)
+#         current = current.next
+    
+#     return dummy.next
+
